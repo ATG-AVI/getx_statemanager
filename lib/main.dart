c3aaa51f913e9709d365a5e5fc734a0333379b6f
Aviral Sharma <imaviralsharma@gmail.com>
2024-08-21 09:10:59 +0530

    fix: Fixed all the bugs

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_getx/next_screen.dart';
-import 'package:flutter_getx/unknown_route.dart';
-import 'package:get/get.dart';
-import 'home.dart';
+import 'package:get/get.dart'; // Import the GetX package
 
-/* If we want that every time the value of count variable changes
-  then all the widgets which uses the count variable must update itself
-  then the count variable must be reactive or observable and to make it
-  Reactive(Rx) .obs is used with variable value.
-  To update the widget which uses Rx variable must be placed inside
-  Obx(() => Your widget which uses Rx)
-The widget will only update if and only if the Rx variable value changes.
-  Other ways of making the variable Rx
-  1 - The first is using Rx{Type}.
-  ===============================
-   initial value is recommended, but not mandatory
-  final name = RxString('');
-  final isLogged = RxBool(false);
-  final count = RxInt(0);
-  final balance = RxDouble(0.0);
-  final items = RxList<String>([]);
-  final myMap = RxMap<String, int>({});
-  2- use Darts Generics, Rx<Type>
-  ===============================
-  final name = Rx<String>('');
-final isLogged = Rx<Bool>(false);
-final count = Rx<Int>(0);
-final balance = Rx<Double>(0.0);
-final number = Rx<Num>(0)
-final items = Rx<List<String>>([]);
-final myMap = Rx<Map<String, int>>({});
-// Custom classes - it can be any class, literally
-final user = Rx<User>();
-  */
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  var count = 0.obs;
+  // Making the count variable reactive by using RxInt
+  final RxInt count = 0.obs;
 
   void increment() {
-    count++;
+    count.value++;
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return GetMaterialApp(
       title: "State Management",
       home: Scaffold(
@@ -55,17 +23,15 @@ class MyApp extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Obx(() => Text(
-                    "Count value is $count",
+                    "Count value is ${count.value}",
                     style: TextStyle(fontSize: 25),
                   )),
               SizedBox(
                 height: 16,
               ),
-              RaisedButton(
+              ElevatedButton(
                 child: Text("Increment"),
-                onPressed: () {
-                  increment();
-                },
+                onPressed: increment,
               ),
             ],
           ),
